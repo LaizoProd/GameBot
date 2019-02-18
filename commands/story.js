486b8d4a6a03ddf216201346386652eb190e3036
Laizo <47483648+LaizoProd@users.noreply.github.com>
2019-02-18 14:04:55 +0100

    Add files via upload

---
+++ commands/story.js
@@ -0,0 +1,38 @@
+const Discord = require("discord.js");
+
+module.exports = class story {
+    constructor() {
+        this.name = 'story',
+            this.alias = ['histoire'],
+            this.usage = '.story'
+    }
+    run(bot, message, args) {
+        message.delete()
+        let setLanguage;
+        if (args[0] === ".story"|| ";story") {
+            setLanguage = require("../language/en.json");
+        } else if (args[0] === ".histoire" || ";histoire") {
+            setLanguage = require("../language/fr.json");
+        }
+        const pbanembed = new Discord.RichEmbed()
+            .setTitle(setLanguage.sTitle)
+            .setColor("#ff0000")
+            .setFooter("GameBot by Laizo", bot.user.avatarURL)
+            .addField(setLanguage.sChooseTitle,
+                setLanguage.sChoose)
+        message.author.send(pbanembed)
+            .then(embedMessage => embedMessage.react("💀").then(() => embedMessage.react("❎"))
+                .then(bot.on("messageReactionAdd", function (reaction, user) {
+                    if (embedMessage.id === reaction.message.id) {
+                        if (user.id == "540103334309265408") {
+                        } else {
+                            if (reaction.emoji.name === "💀") {
+                                message.author.send("ça commence...")
+                            } else {
+                            }
+                        }
+                    }
+                }))
+            )
+    }
+};
