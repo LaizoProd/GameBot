62bfb31a8bf9d1c97371059dba258014e9a89d89
Laizo <47483648+LaizoProd@users.noreply.github.com>
2019-04-20 21:55:37 +0200

    Update punban.js

--- commands/punban.js
+++ commands/punban.js
@@ -4,15 +4,14 @@ module.exports = class punban {
             this.alias = ['punban'],
             this.usage = '.partyunban'
     }
-    run(bot, message, args, fr, en, setLanguage) {
+    run(bot, message, args, setLanguage, db) {
         message.delete()
         if (message.member.hasPermission("ADMINISTRATOR")) {
-            if (fr.has(message.guild.id) || en.has(message.guild.id)) {
+            if (db.get(message.guild.id).__wrapped__[message.guild.id] === "fr" || db.get(message.guild.id).__wrapped__[message.guild.id] === "en") {
                 if (!message.guild.roles.find(role => role.name === "pban") == undefined) {
                     let member = message.mentions.members.first();
                     if (member) {
-                        var role = message.guild.roles.find(role => role.name === "pban")
-                        member.removeRole(role)
+                        member.removeRole(message.guild.roles.find(role => role.name === "pban"))
                         message.channel.send(`${member} ${setLanguage.bAnnounce}`)
                     } else {
                         message.author.send(setLanguage.bNoPlayer)
